48095abe998c79c4bb964d3db82555b2fd7302f5
“WarTey” <guillaume.blanc-de-lanaute@isen.yncrea.fr>
2020-09-28 05:34:14 +0200

    Ajout du premier personnage

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    protected override void Update()
+    {
+        GetInput();
+
+        base.Update();
+    }
+
+    private void GetInput()
+    {
+        direction = Vector2.zero;
+
+        transform.eulerAngles = new Vector3(0, 360, 0);
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            // Animation gauche manquante
+            direction += Vector2.right;
+
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+    }
+}
